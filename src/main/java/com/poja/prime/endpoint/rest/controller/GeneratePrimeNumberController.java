f893ce0405bced4e271567ab07f68ddeff055634
Daris02 <hei.raymond.2@gmail.com>
2023-12-12 18:49:21 +0300

    fix: reduce bit count

--- src/main/java/com/poja/prime/endpoint/rest/controller/GeneratePrimeNumberController.java
+++ src/main/java/com/poja/prime/endpoint/rest/controller/GeneratePrimeNumberController.java
@@ -16,6 +16,6 @@ public class GeneratePrimeNumberController {
     public BigInteger generatePrime() {
         SecureRandom random = new SecureRandom();
 
-        return new BigInteger(5_000, 99, random);
+        return new BigInteger(2_500, 99, random);
     }
 }
